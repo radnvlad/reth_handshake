77d8adfcf81ec16af74bed88ec69f41c2a883945
Vlad Radulescu <vlad.here@gmail.com>
2024-10-02 00:23:35 +0300

    Apparently the from_str isn't working great to get the key.

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ fn main() {
         Err(e) => error!("Error getting peers! {}",e)
 
     }
-    debug!("Hey Hey Hey");
 }
 
 // fn get_peers() -> Result<(Vec<(secp256k1::PublicKey, String)>)>
@@ -18,16 +17,10 @@ fn get_peers() -> Result<(PublicKey, String), &'static str>
     const ENODE_PREFIX:&str = "enode://";
 
     let mut nodes: Vec<(PublicKey, String)> = Vec::new();
-    // let mut enode_public_key: PublicKey;
-    // iter = args.next()
+
     for enode in env::args().skip(1){
 
         debug!("Args are: {:?}", enode);
-
-        // if !enode.starts_with(ENODE_PREFIX) {
-        //     error!("Invalid enode prefix for {:?}", enode);
-        //     return Err("Invalid enode prefix");
-        // }
 
         let (enode_prefix, enode_data) = 
         match enode.split_once(ENODE_PREFIX) {
@@ -45,17 +38,17 @@ fn get_peers() -> Result<(PublicKey, String), &'static str>
                 None => return Err("Invalid ip address"),
             };
 
+        println!("key string is: {:?}", enode_key_string);
+
         let enode_public_key = match PublicKey::from_str(&enode_key_string){
             Ok(e) => e,
-            _ => {return Err("Invalid enode public key ")}
+            Err(x) => {
+                println!("Key parse err is: {:?}", x);
+
+                return Err("Invalid enode public key ")}
         };
         nodes.push((enode_public_key, ip_address.to_string()));
-        // let ip_address = enode
 
-        // if !enode.starts_with("enode://") {
-        //     error!("Invalid enode prepend for {:?}", enode);
-        //     return Err("Invalid enode prepend");
-        // }
     }
     // nodes
     Err("Placeholder! ")
