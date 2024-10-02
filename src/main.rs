583ac26c5194c0ad0a7afeb3d0bf58d2eb6f1bb0
Vlad Radulescu <vlad.here@gmail.com>
2024-10-02 14:51:42 +0300

    Propagate socket addr instead of string.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use log::{error, info, warn, debug};
 use secp256k1::{PublicKey, SecretKey};
-use std::{env, fmt::Error, net::TcpStream, net::IpAddr, str::FromStr};
+use std::{env, fmt::Error, net::{SocketAddr, TcpStream, ToSocketAddrs}, str::FromStr};
 
 fn main() {
     env_logger::init();
@@ -15,11 +15,11 @@ fn main() {
     }
 }
 
-fn get_peers() -> Result<Vec<(PublicKey, String)>, &'static str>
+fn get_peers() -> Result<Vec<(PublicKey, SocketAddr)>, &'static str>
 {
     const ENODE_PREFIX:&str = "enode://";
 
-    let mut nodes: Vec<(PublicKey, String)> = Vec::new();
+    let mut nodes: Vec<(PublicKey, SocketAddr)> = Vec::new();
 
     for enode in env::args().skip(1){
 
@@ -32,15 +32,19 @@ fn get_peers() -> Result<Vec<(PublicKey, String)>, &'static str>
             return Err("Invalid enode prefix location! ")
         }
 
-        let (enode_key_string, ip_address) = enode_data
+        let (enode_key_string, socket_address_string) = enode_data
             .rsplit_once("@")
-            .ok_or("Invalid ip address")?;
+            .ok_or("Invalid ip delimiter")?;
 
-        if false == IpAddr::from_str(ip_address)
-            .map_err(|_|" Invalid IP address format! ")?
-            .is_ipv4() {
-                return Err("IP address is not IPv4! ")
-            }
+        let mut socket_address = socket_address_string
+            .to_socket_addrs()
+            .inspect_err(|err| debug!("The IpAddr conversion parse error is {:?}.", err))
+            .map_err(|_|" Invalid IP address format! ")?;
+
+        if socket_address.len() !=1 {return Err("Multiple IP/sockets specified for node, only one 1 supported! ")}
+        
+        let socket_address = socket_address.next()
+            .ok_or("Invalid IP address! ")?;
 
         let mut enode_key_string =   enode_key_string.to_string();
         enode_key_string.insert_str(0, "04");
@@ -49,16 +53,19 @@ fn get_peers() -> Result<Vec<(PublicKey, String)>, &'static str>
             .inspect_err(|err| debug!("The publickey parse error is {:?}.", err))
             .map_err(|_|"Invalid enode public key ")?;
         
-        nodes.push((enode_public_key, ip_address.to_string()));
+        nodes.push((enode_public_key, socket_address));
     }
     Ok(nodes)
 }
 
 
-fn establish_session(public_key: PublicKey, ip_address: String)
+fn establish_session(public_key: PublicKey, socket_address: SocketAddr)
 {
-    match TcpStream::connect(&ip_address) {
-        Ok(mut stream) => {}
-        Err(e) => {}
+    match TcpStream::connect(&socket_address) {
+        Ok(mut stream) => {
+            info!("TCP stream established! ");}
+        Err(e) => {
+            info!("TCP stream failed! ");
+        }
     }
 }
