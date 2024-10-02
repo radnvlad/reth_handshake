16862adff004d90a9f818e2b1ab41ad76561f5a8
Vlad Radulescu <vlad.here@gmail.com>
2024-10-02 13:21:22 +0300

    Change a bit the argument parsing structure.

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ fn get_peers() -> Result<Vec<(PublicKey, String)>, &'static str>
 
     for enode in env::args().skip(1){
 
-        debug!("Args are: {:?}", enode);
+        debug!("Enode argument is: {:?}", enode);
 
         let (enode_prefix, enode_data) = 
         match enode.split_once(ENODE_PREFIX) {
@@ -35,22 +35,20 @@ fn get_peers() -> Result<Vec<(PublicKey, String)>, &'static str>
             return Err("Invalid enode prefix location! ")
         }
 
-        let (enode_key_string, ip_address) = 
-            match enode_data.rsplit_once("@") {
-                Some(x) => x,
-                None => return Err("Invalid ip address"),
-            };
+        let (enode_key_string, ip_address_string) = enode_data
+            .rsplit_once("@")
+            .ok_or("Invalid ip address")?;
+
+        let ip_address = IpAddr::from_str(ip_address_string)
+            .map_err(|_|" Invalid IP address format! ")?;
 
         let mut enode_key_string =   enode_key_string.to_string();
         enode_key_string.insert_str(0, "04");
 
-        let enode_public_key = match PublicKey::from_str(&enode_key_string){
-            Ok(e) => e,
-            Err(x) => {
-                error!("Key parse err is: {:?}", x);
-                return Err("Invalid enode public key ")
-            }
-        };
+        let enode_public_key = PublicKey::from_str(&enode_key_string)
+            .inspect_err(|err| debug!("The publickey parse error is {:?}.", err))
+            .map_err(|_|"Invalid enode public key ")?;
+        
         nodes.push((enode_public_key, ip_address.to_string()));
     }
     Ok(nodes)
